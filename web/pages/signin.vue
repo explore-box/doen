297977889826fcf5b091a47e7a55ce17523ba2b0
nyomansunima <nyomansunima@gmail.com>
2023-04-29 15:02:05 +0800

    👏 update: add the back button and headline

--- web/pages/signin.vue
+++ web/pages/signin.vue
@@ -1,5 +1,15 @@
 <!-- signin page -->
 <!-- use to handle the user signin data and login to app -->
+
+<script setup lang="ts">
+const router = useRouter()
+
+const back = () => {
+  console.log('Back')
+  router.back()
+}
+</script>
+
 <template>
   <div>
     <Head>
@@ -12,14 +22,19 @@
 
     <main class="flex flex-col">
       <section class="flex bg-white h-full w-full px-20 relative">
-        <div class="flex flex-col w-1/2">
-          <div class="flex">
-            <ButtonText>Hello from template design</ButtonText>
-          </div>
+        <div class="flex flex-col w-1/2 py-28">
+          <ButtonText @click="back" class="text-gray-400 fixed top-10 left-14">
+            <i class="fi fi-rr-arrow-left"></i>
+            Back
+          </ButtonText>
+
+          <h2
+            class="flex w-9/12 text-7xl font-semibold font-recoleta text-brand-purple-dark mt-10 leading-tight"
+          >
+            Login with single click
+          </h2>
         </div>
       </section>
     </main>
   </div>
 </template>
-
-<script setup lang="ts"></script>
