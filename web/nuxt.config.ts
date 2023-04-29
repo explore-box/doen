a4d9dfeb4aed0962846a4a87fcf3e5ced8c6585b
nyomansunima <nyomansunima@gmail.com>
2023-04-29 11:24:53 +0800

    👏 update: add the global css, flaticon css

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,11 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxtjs/tailwindcss'],
+  css: [
+    '@/assets/css/globals.css',
+    '@flaticon/flaticon-uicons/css/all/all.css',
+  ],
   typescript: {
     shim: false,
     strict: true,
   },
   routeRules: {
-    "/": { ssr: true },
-    "/app/**": { ssr: false },
+    '/': { ssr: true },
+    '/app/**': { ssr: false },
   },
-});
+})
