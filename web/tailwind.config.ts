688e559ff439b41e9d3dca6fcd47d859fbcd729f
nyomansunima <nyomansunima@gmail.com>
2023-04-29 15:02:35 +0800

    🍀 update: add the tailwind plugins

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -1,5 +1,8 @@
 import type { Config } from 'tailwindcss'
 
+import forms from '@tailwindcss/forms'
+import acpectRatio from '@tailwindcss/aspect-ratio'
+
 /**
  * # Tailwindcss Configuration file
  */
@@ -7,7 +10,7 @@ export default <Partial<Config>>{
   theme: {
     fontFamily: {
       'dm-sans': ['DM Sans', 'sans-serif'],
-      recoleta: ['Recoleta', 'serif'],
+      recoleta: ['Recoleta', 'serif']
     },
     extend: {
       colors: {
@@ -15,9 +18,10 @@ export default <Partial<Config>>{
           pink: '#FFB9F8',
           purple: '#6532F8',
           black: '#0E0D10',
-          'purple-dark': '#1A094B',
-        },
-      },
-    },
+          'purple-dark': '#1A094B'
+        }
+      }
+    }
   },
+  plugins: [forms, acpectRatio]
 }
