8cc14c3eb92c821fd2f877f77d7c6e580790af0d
nyomansunima <nyomansunima@gmail.com>
2023-05-01 10:34:20 +0800

    🥝 chore: update the main configuration and also create swagger document

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,69 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import * as compression from 'compression'
+import helmet from 'helmet'
+import {
+  INestApplication,
+  Logger,
+  ValidationPipe,
+  VersioningType
+} from '@nestjs/common'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+// define app props
+const PORT = parseInt(process.env.PORT, 10) || 4000
+
+/**
+ * # createOpenAPIDocument
+ *
+ * create the openAI Swagger document
+ * @param app the nestjs application
+ */
+function createOpenAPIDocument(app: INestApplication) {
+  const logger = new Logger('OpenAPI')
+
+  logger.log(`🍀 Creating the OpenAPI Document`)
+  const config = new DocumentBuilder()
+    .setTitle('DOEN API')
+    .addTag('api', 'The backend api')
+    .addBearerAuth()
+    .addOAuth2()
+    .setDescription(
+      'Doen Application plaform Backend API. Integrations, and provide service data'
+    )
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('/', app, document)
+
+  logger.log(`🎉 Open API Document Created`)
 }
-bootstrap();
+
+/**
+ * # bootstrap
+ *
+ * start the application
+ * by bootstrap all of the resources
+ */
+async function bootstrap() {
+  const logger = new Logger('APP')
+  const app = await NestFactory.create(AppModule)
+
+  // register the app security, plugins,
+  // and other middleware
+  app.enableVersioning({ type: VersioningType.URI })
+  app.useGlobalPipes(new ValidationPipe({}))
+  app.use(compression())
+  app.use(helmet())
+  app.enableCors({
+    origin: '*'
+  })
+
+  // create swagger document
+  createOpenAPIDocument(app)
+
+  await app.listen(PORT, () => {
+    logger.log(`🎉 Application running on port ${PORT}`)
+  })
+}
+
+bootstrap()
