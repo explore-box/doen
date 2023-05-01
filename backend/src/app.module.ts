7db2d742b1a1143a5bf511d36445d4566809a3af
nyomansunima <nyomansunima@gmail.com>
2023-05-01 10:31:23 +0800

    🎉 update: adding the config, throttler, tigris and also feature modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,31 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { TigrisModule } from './tigris/tigris.module'
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler'
+import { APP_GUARD } from '@nestjs/core'
+import { UserSchema } from './user/model/user.schema'
+import { UserModule } from './user/user.module'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, expandVariables: true }),
+    ThrottlerModule.forRoot({
+      limit: 10,
+      ttl: 60
+    }),
+    TigrisModule.forRoot({ schemas: [UserSchema] }),
+
+    // feature module
+    UserModule,
+    AuthModule
+  ],
+
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    }
+  ]
 })
 export class AppModule {}
