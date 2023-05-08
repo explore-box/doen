d8c0d06ac2f0cd66b69d1e6afcf77927b2387b98
nyomansunima <nyomansunima@gmail.com>
2023-05-08 12:07:20 +0800

    🎉 build: create app layout

---
+++ web/layouts/app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <!-- header -->
+    <AppHeader />
+
+    <!-- side navigations -->
+    <AppSideNav />
+
+    <!-- the app childs component -->
+    <main class="flex">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
