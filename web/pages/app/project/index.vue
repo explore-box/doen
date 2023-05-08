fd970489f8960fb82afe9b6eabde4f0cdb679674
nyomansunima <nyomansunima@gmail.com>
2023-05-08 12:07:42 +0800

    🎉 build: create the project page

---
+++ web/pages/app/project/index.vue
@@ -0,0 +1,25 @@
+<!-- Project List Page -->
+<!-- list all project and show the current status -->
+
+<template>
+  <Head>
+    <Title>Projects | Doen</Title>
+    <Meta name="description"
+      >Explore all the project and start collaborates</Meta
+    >
+  </Head>
+
+  <div class="flex"></div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'app'
+})
+</script>
+
+<style>
+body {
+  @apply !bg-slate-50;
+}
+</style>
