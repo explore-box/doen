996d2bd6a742b308170fee319933ac77f6715fd9
nyomansunima <nyomansunima@gmail.com>
2023-05-08 12:06:27 +0800

    🎉 build: create the app header

---
+++ web/components/header/AppHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <header
+    id="app-header"
+    class="header flex fixed top-0 inset-x-0 items-center"
+  >
+    <div class="flex flex-1"></div>
+    <!-- menus -->
+    <ul class="flex items-center list-none">
+      <li class="flex justify-center items-center"></li>
+    </ul>
+
+    <div>
+      <NuxtImg />
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
