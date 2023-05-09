5d5dd27fe5b45e9997ffd530a55b6931f777be95
nyomansunima <nyomansunima@gmail.com>
2023-05-09 16:35:55 +0800

    🔥 update: adding the filter, title, and project info

--- web/pages/app/project/index.vue
+++ web/pages/app/project/index.vue
@@ -9,12 +9,38 @@
     >
   </Head>
 
-  <div class="flex"></div>
+  <div class="flex w-full">
+    <!-- detail and all project show here -->
+    <div class="flex flex-col w-9/12">
+      <h3 class="flex text-4xl font-medium">Projects</h3>
+
+      <!-- Filter and sorts -->
+      <div class="flex items-center mt-7 gap-4">
+        <ProjectStatusFilter />
+        <ProjectSortFilter />
+        <ProjectSearchFilter />
+        <FlatButton class="bg-white !h-[52px] !w-[52px] !p-0">
+          <i class="fi fi-rr-apps text-xl"></i>
+        </FlatButton>
+      </div>
+
+      <!-- list of projects -->
+      <div class="grid grid-cols-3 gap-x-5 gap-y-10 mt-10">
+        <template v-for="index in 10">
+          <ProjectGridItem />
+        </template>
+      </div>
+    </div>
+
+    <!-- this is showing the user project info -->
+    <!-- must as fixed -->
+    <ProjectSideInfo />
+  </div>
 </template>
 
 <script setup lang="ts">
 definePageMeta({
-  layout: 'app'
+  layout: 'main'
 })
 </script>
 
