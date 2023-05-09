892ecdd08b0ea21a35e9afaee5d6aa163dc83992
nyomansunima <nyomansunima@gmail.com>
2023-05-09 16:02:22 +0800

    🔥 update: adding the nuxt layout component

--- web/app.vue
+++ web/app.vue
@@ -1,14 +1,16 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+
 <script setup lang="ts">
 useHead({
   bodyAttrs: {
     class:
-      'font-dm-sans font-base leading-normal bg-white text-black font-normal selection:bg-brand-pink selection:text-brand-purple-dark'
+      'font-dm-sans text-base leading-normal bg-white text-black font-normal selection:bg-brand-pink selection:text-brand-purple-dark'
   }
 })
 </script>
-
-<template>
-  <div>
-    <NuxtPage />
-  </div>
-</template>
