574292e63403c3e42ddc58325968a598e09e72c6
nyomansunima <nyomansunima@gmail.com>
2023-05-14 12:32:23 +0800

    🎉 build: create the outline type button

---
+++ web/components/button/OutlineButton.vue
@@ -0,0 +1,17 @@
+<!-- Outline Button -->
+<template>
+  <button
+    type="button"
+    class="flex justify-center items-center gap-3 px-4 h-11 text-base rounded-full border border-gray-100 outline-button transition-all duration-300 hover:-translate-y-1"
+  >
+    <slot>Button</slot>
+  </button>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+:slotted(i) {
+  @apply text-lg;
+}
+</style>
