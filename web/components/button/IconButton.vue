5032458a32db3fcb66fca1d6cd831060348c2a00
nyomansunima <nyomansunima@gmail.com>
2023-05-14 12:32:08 +0800

    🎉 build: create the icon button

---
+++ web/components/button/IconButton.vue
@@ -0,0 +1,30 @@
+<!-- Icon Button -->
+<template>
+  <button
+    type="button"
+    class="flex justify-center items-center rounded-2xl transition-all duration-300 hover:-translate-y-1"
+    :class="[mappedClass, props.class]"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+interface IconButtonProps {
+  class?: string
+  showBackground?: boolean
+}
+const props = withDefaults(defineProps<IconButtonProps>(), {
+  showBackground: true
+})
+
+const mappedClass = computed(() => {
+  return `${props.showBackground ? 'h-11 w-11 bg-gray-100' : '!bg-transparent'}`
+})
+</script>
+
+<style scoped>
+:slotted(i) {
+  @apply text-lg;
+}
+</style>
