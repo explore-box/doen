8a0765682d8e127f18f12fbfb4e86f6e883d853b
nyomansunima <nyomansunima@gmail.com>
2023-05-14 12:33:52 +0800

    🎉 build: create the detail project page

---
+++ web/pages/app/project/[...slug].vue
@@ -0,0 +1,17 @@
+<!-- Detail Projects -->
+<!-- Including the project detail info and open the tasks in tray -->
+<template>
+  <div class="w-full h-full">
+    <!-- project header -->
+    <ProjectDetailHeader />
+
+    <!-- all tasks views -->
+    <ProjectView />
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'main'
+})
+</script>
